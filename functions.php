33e6acecc3c30dbacc0746362714f93cc0bb4f46
Prof. Leighton Rodney <rodneyl@Leightons-MBP.localdomain>
2024-07-29 08:45:22 -0400

    edited functions.php file

--- functions.php
+++ functions.php
@@ -12,10 +12,6 @@ function blank_theme_setup() {
     
     // Add support for full and wide align images
     add_theme_support('align-wide');
-    
-    // Add support for editor styles
-    add_theme_support('editor-styles');
-    add_editor_style('style-editor.css'); // You can create a custom stylesheet for the editor
 
     // Add support for responsive embedded content
     add_theme_support('responsive-embeds');
