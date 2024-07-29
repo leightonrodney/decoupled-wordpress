e2083d3e8c971e924018df13d37fdd4542c2d933
Prof. Leighton Rodney <rodneyl@Leightons-MBP.localdomain>
2024-07-29 08:44:42 -0400

    added functions.php file

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+// Enqueue styles
+function blank_theme_enqueue_styles() {
+    wp_enqueue_style('blank-theme-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'blank_theme_enqueue_styles');
+
+// Add theme support for various features
+function blank_theme_setup() {
+    // Add support for block styles
+    add_theme_support('wp-block-styles');
+    
+    // Add support for full and wide align images
+    add_theme_support('align-wide');
+    
+    // Add support for editor styles
+    add_theme_support('editor-styles');
+    add_editor_style('style-editor.css'); // You can create a custom stylesheet for the editor
+
+    // Add support for responsive embedded content
+    add_theme_support('responsive-embeds');
+    
+    // Add support for custom line height controls
+    add_theme_support('custom-line-height');
+    
+    // Add support for custom units
+    add_theme_support('custom-units');
+}
+add_action('after_setup_theme', 'blank_theme_setup');
