f77a3e3a0d6efac8a292669842767a11fb319fe9
Prof. Leighton Rodney <rodneyl@Leightons-MBP.localdomain>
2024-07-29 08:50:19 -0400

    edited index.php file

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
+get_header(); // Include header.php
+?>
 
-// Silence is truly Golden. 
+<main>
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            the_content(); // Display the content of the post
+        endwhile;
+    else :
+        echo '<p>No content found</p>';
+    endif;
+    ?>
+</main>
+
+<?php
+get_footer(); // Include footer.php
+?>
